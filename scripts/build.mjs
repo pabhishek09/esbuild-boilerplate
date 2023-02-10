41c5f56eb749b37ce0a06ca11fc37a660c978922
Naveen Arora <naveenarora@deloitte.co.uk>
2023-02-10 10:29:27 +0000

    sass compiler added

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,57 +2,59 @@
   Bundle components for AEM
  */
 
-import { resolve, dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-import { mkdirSync } from 'fs';
-import { readFile, readdir, writeFile } from 'fs/promises';
-import * as esbuild from 'esbuild';
-import handlebars from 'handlebars';
+import { resolve, dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { mkdirSync } from "fs";
+import { readFile, readdir, writeFile } from "fs/promises";
+import * as esbuild from "esbuild";
+import handlebars from "handlebars";
+import { sassPlugin } from "esbuild-sass-plugin";
 
 const currDir = dirname(fileURLToPath(import.meta.url));
-const componentsDir = resolve(currDir, '../', 'src', 'components');
-const buildDir = resolve(currDir, '../', 'dist');
+const componentsDir = resolve(currDir, "../", "src", "components");
+const buildDir = resolve(currDir, "../", "dist");
 
 const createMarkup = async (componentDir) => {
-  const srcTemplate = await readFile(join(componentsDir, componentDir, 'template.hbs'), 'utf-8');
+  const srcTemplate = await readFile(
+    join(componentsDir, componentDir, "template.hbs"),
+    "utf-8"
+  );
   const compiledTemplate = handlebars.compile(srcTemplate);
-  const writePath = join(buildDir, componentDir, 'temlplate.html');
-  await writeFile(writePath, compiledTemplate({ artist: 'Beatles' }), { flag: 'w' });
-}
- 
+  const writePath = join(buildDir, componentDir, "temlplate.html");
+  await writeFile(writePath, compiledTemplate({ artist: "Beatles" }), {
+    flag: "w",
+  });
+};
 
-
-const buildComponents = async() => {
-
+const buildComponents = async () => {
   const components = await readdir(componentsDir);
 
   components.forEach((componentDir) => {
-
     // create output directory
-    const outputDir = resolve(currDir, '../', 'dist', componentDir);
+    const outputDir = resolve(currDir, "../", "dist", componentDir);
     mkdirSync(outputDir, { recursive: true });
 
     // dist/<component>/template.html
     createMarkup(componentDir);
-    
+
     // dist/<component>/script.js
-    esbuild.build({
-      bundle: true,
-      entryPoints: [join(componentsDir, componentDir, 'script.mjs')],
-      outdir: outputDir,
-      plugins: []
-    })
-    .then(console.log)
-    .catch(() => process.exit(1))
+    esbuild
+      .build({
+        bundle: true,
+        entryPoints: [
+          join(componentsDir, componentDir, "script.mjs"),
+          join(componentsDir, componentDir, "style.scss"),
+        ],
+        outdir: outputDir,
+        plugins: [
+          sassPlugin(),
+        ],
+      })
+      .then(console.log)
+      .catch(() => process.exit(1));
 
     // dist/<component>/styles.css - generate css from scss
-
-
   });
+};
 
-
-}
-
-buildComponents()
-  .then(console.log)
-  .catch(console.error);
+buildComponents().then(console.log).catch(console.error);
